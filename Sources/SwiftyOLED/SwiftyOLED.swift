ef2906cf7c1af751a7b635e86eb7dda2c1158879
3Qax <kuba@towarek.pl>
2019-06-29 16:39:04 +0200

    Support different display sizes, rename some methods and fix Accsess Control

--- Sources/SwiftyOLED/SwiftyOLED.swift
+++ Sources/SwiftyOLED/SwiftyOLED.swift
@@ -1,19 +1,140 @@
 import SwiftyGPIO
-#if os(macOS) || os(iOS)
-import Darwin
-#elseif os(Linux) || CYGWIN
-import Glibc
-#endif
 
 
 
 public final class OLED  {
     
-    private var buffer = Array<UInt8>(repeating: 0, count: 128*(32/8))
     private let i2c: I2CInterface
     private let address: Int
+    private let width: UInt
+    private let height: UInt
+    private var buffer: Array<UInt8>
     
-    enum Command: UInt8 {
+    public enum Brightness {
+        case dimmed
+        case bright
+        case custom(value: UInt8)
+    }
+    
+    public enum State {
+        case on
+        case off
+    }
+    
+    
+    
+    public init(connectedTo interface: I2CInterface, at address: Int, width: UInt, height: UInt) {
+        self.i2c = interface
+        self.address = address
+        self.width = width
+        self.height = height
+        self.buffer = Array<UInt8>(repeating: 0, count: Int(width*(height/8)))
+        initialization()
+    }
+    
+    //This method MUST be called when initialazing
+    //It performs neccessary setup
+    //Please refer to page 10 (section 4.4) of UG-2832HSWEG02 datasheet for more info.
+    internal func initialization() {
+        self.turn(.off)
+        send(command: .SetDisplayClockDiv)
+        send(customCommand: 0x80)                                  // the suggested ratio 0x80
+        send(command: .SetMultiplex)
+        send(customCommand: 0x1F)
+        send(command: .SetDisplayOffset)
+        send(customCommand: 0x0)                                   // no offset
+        send(customCommand: Command.SetStartLine.rawValue | 0x0)   // line #0
+        send(command: .ChargePump)
+        send(customCommand: 0x14)
+        send(command: .MemoryMode)                                  // 0x20
+        send(customCommand: 0x00)                                  // 0x0 act like ks0108
+        send(customCommand: Command.SEGREMAP.rawValue | 0x1)
+        send(command: .COMSCANDEC)
+        send(command: .SetComPins)
+        send(customCommand: 0x02)
+        self.set(brightness: .custom(value: UInt8(0x8F)))
+        send(command: .SetPrecharge)
+        send(customCommand: 0xF1)
+        send(command: .SetVComDetect)
+        send(customCommand: 0x40)
+        send(command: .DisplayAllOnResume)
+        send(command: .NormalDisplay)
+        self.turn(.on)
+    }
+    
+    //Makes point (tuplet of x and y coordinates) white
+    //Drawing outside screen does not fail
+    public func draw(point: (Int, Int)) {
+        //if given point is in display's range draw it
+        if point.0 >= 0 && point.0 <= width-1
+        && point.1 >= 0 && point.1 <= height-1 {
+            buffer[Int(point.1/8)*Int(width)+Int(point.0)] |=  1<<(point.1%8)
+        }
+    
+    }
+    
+    //Makes points (arrary of tuplets (x and y coordinates)) white
+    //by calling draw(point:_) for each point
+    public func draw(points: [(Int, Int)]) {
+        
+        for point in points {
+            // point.0 is x of point
+            // point.1 is y of point
+            self.draw(point: (point.0, point.1))
+        }
+        
+    }
+    
+    //Makes the entire buffer white
+    public func fill() {
+        buffer = Array<UInt8>(repeating: UInt8.max, count: Int(width*(height/8)))
+    }
+    
+    //Makes the entire buffer black
+    public func clear() {
+        buffer = Array<UInt8>(repeating: 0, count: Int(width*(height/8)))
+    }
+    
+    //Makes data from buffer appear on physical display
+    public func display() {
+        send(command: Command.ColumnAddr)
+        send(customCommand: 0)
+        send(customCommand: UInt8(width-1))
+        send(command: Command.PageAddr)
+        send(customCommand: 0)
+        send(customCommand: UInt8((height/8)-1))
+        sendBuffer()
+    }
+    
+    public func set(brightness: Brightness) {
+        switch brightness {
+        case .dimmed:
+            send(command: .SetContrast)
+            send(customCommand: 0x00)
+        case .bright:
+            send(command: .SetContrast)
+            send(customCommand: 0xCF)
+        case .custom(let value):
+            send(command: .SetContrast)
+            send(customCommand: value)
+        }
+    }
+    
+    public func turn(_ state: State) {
+        switch state {
+        case .on:
+            send(command: .DisplayOn)
+        case .off:
+            send(command: .DisplayOff)
+        }
+    }
+    
+}
+
+//Extension defining commands and methods to send them to display
+extension OLED {
+    
+    internal enum Command: UInt8 {
         case SetContrast                            = 0x81
         case DisplayAllOnResume                     = 0xA4
         case DisplayAllOn                           = 0xA5
@@ -47,154 +168,38 @@ public final class OLED  {
         case VerticalAndRightHorizontalScroll       = 0x29
         case VerticalAndLeftHorizontalScroll        = 0x2A
     }
-    public enum Brightness {
-        case dimmed
-        case bright
-        case custom(value: UInt8)
-    }
-    public enum State {
-        case on
-        case off
-    }
     
-    
-    
-    //Please refere to page 10 (section 4.4) of UG-2832HSWEG02 datasheet for more info.
-    func initialization() {
-        self.turn(.off)
-        send(command: .SetDisplayClockDiv)
-        send(customCommand: 0x80)                                  // the suggested ratio 0x80
-        send(command: .SetMultiplex)
-        send(customCommand: 0x1F)
-        send(command: .SetDisplayOffset)
-        send(customCommand: 0x0)                                   // no offset
-        send(customCommand: Command.SetStartLine.rawValue | 0x0)   // line #0
-        send(command: .ChargePump)
-        send(customCommand: 0x14)
-        send(command: .MemoryMode)                                  // 0x20
-        send(customCommand: 0x00)                                  // 0x0 act like ks0108
-        send(customCommand: Command.SEGREMAP.rawValue | 0x1)
-        send(command: .COMSCANDEC)
-        send(command: .SetComPins)
-        send(customCommand: 0x02)
-        self.setBrightness(.custom(value: UInt8(0x8F)))
-        send(command: .SetPrecharge)
-        send(customCommand: 0xF1)
-        send(command: .SetVComDetect)
-        send(customCommand: 0x40)
-        send(command: .DisplayAllOnResume)
-        send(command: .NormalDisplay)
-        self.turn(.on)
-    }
-    
-    public init(connectedTo interface: I2CInterface, at address: Int) {
-        self.i2c = interface
-        self.address = address
-        initialization()
-    }
-    
-    //Makes pixel at given coordinates white
-    //Works in iPhone like coordinate system
-    public func drawPixel(x: UInt8, y: UInt8){
-        //since swift does not offer pow(Int, Int) conversion to double and back to UInt8 is necessary
-        buffer[Int(y/8)*128+Int(x)] |= UInt8(pow(Double(2), Double(y%8)))
-    }
-    
-    //Draw objet (arrary of tuplets (x and y points)) at given origin
-    //Drawing outside screen is possible
-    public func draw(points: [(Int, Int)], at origin: (Int, Int) = (0, 0)) {
-        
-        // origin.0 is x of origin
-        // origin.1 is y of origin
-        for point in points {
-            // point.0 is x of point
-            // point.1 is y of point
-            
-            //if given point is in display's range draw it
-            if point.0+origin.0 >= 0 && point.0+origin.0 <= 127
-            && point.1+origin.1 >= 0 && point.1+origin.1 <= 31 {
-                self.drawPixel(x: UInt8(point.0+origin.0), y: UInt8(point.1+origin.1))
-            }
-        }
-        
-    }
-    
-    //Set the entire display buffer to white
-    public func fill() {
-        buffer = [UInt8](repeating: UInt8.max, count: 128*(32/8))
-    }
-    
-    //Set the entire display buffer to black
-    public func clear() {
-        buffer = [UInt8](repeating: 0, count: 128*(32/8))
-    }
-    
-    //Writes display buffer to physical display
-    public func display() {
-        send(command: Command.ColumnAddr)
-        send(customCommand: 0)
-        send(customCommand: 127)
-        send(command: Command.PageAddr)
-        send(customCommand: 0)
-        send(customCommand: 3)
-        sendBuffer()
-    }
-    
-    //Sets brightness of display to either .dimmed or .bright
-    public func setBrightness(_ brightness: Brightness) {
-        switch brightness {
-        case .dimmed:
-            send(command: .SetContrast)
-            send(customCommand: 0x00)
-        case .bright:
-            send(command: .SetContrast)
-            send(customCommand: 0xCF)
-        case .custom(let value):
-            send(command: .SetContrast)
-            send(customCommand: value)
-        }
-    }
-    
-    public func turn(_ state: State) {
-        switch state {
-        case .on:
-            send(command: .DisplayOn)
-        case .off:
-            send(command: .DisplayOff)
-        }
-    }
-    
-}
-
-//Extension for handling data transfers to display
-extension OLED {
-    func send(command: Command) {
+    internal func send(command: Command) {
         i2c.writeByte(self.address, command: 0b00000000, value: command.rawValue) //Co=0 D/C#=0
     }
     
-    func send(customCommand: UInt8) {
+    internal func send(customCommand: UInt8) {
         i2c.writeByte(self.address, command: 0b00000000, value: customCommand) //Co=0 D/C#=0
     }
-    
+}
+
+//Extension for sending buffer to display
+extension OLED {
+
     //This method is slow, use it only when neccessary!
     //Usage of sendBuffer() is higly recommended
-    func sendBufferByteByByte() {
+    internal func sendBufferByteByByte() {
         for pageColumn in buffer {
             i2c.writeByte(self.address, command: 0b01000000, value: pageColumn) //Co=0 D/C#=1
         }
     }
     
-    func sendBuffer() {
+    internal func sendBuffer() {
         
         //send packages of 32 Bytes, which is I2C max number of bytes send at once
         let numberOfFullPackets = buffer.count/32
         for i in 1...numberOfFullPackets {
-            i2c.writeI2CData(self.address, command: 0b01000000, values: Array(buffer[(i-1)*32...(i*32)-1]))
+            i2c.writeI2CData(self.address, command: 0b01000000, values: Array(buffer[(i-1)*32...(i*32)-1])) //Co=0 D/C#=1
         }
         
         //if there aren't enought bytes left to form a full (32 Bytes) package send them
         if numberOfFullPackets*32 != buffer.count {
-            i2c.writeI2CData(self.address, command: 0b01000000, values: Array(buffer[numberOfFullPackets*32...buffer.count-1]))
+            i2c.writeI2CData(self.address, command: 0b01000000, values: Array(buffer[numberOfFullPackets*32...buffer.count-1])) //Co=0 D/C#=1
         }
     }
     
